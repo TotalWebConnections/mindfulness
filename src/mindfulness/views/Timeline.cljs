8e481d001adae638d877d3fc734568cf485f3a0f
peterjewicz <peterjewicz@totalwebconnections.com>
2020-03-31 15:34:31 -0500

    setup for slider

--- src/mindfulness/views/Timeline.cljs
+++ src/mindfulness/views/Timeline.cljs
@@ -1,17 +1,22 @@
 (ns mindfulness.views.timeline
   (:require [mindfulness.services.state.dispatcher :refer [handle-state-change]]))
 
+(def entry-sizing 200)
+(def entry-padding 75)
+(def base-width 375) ;iosx change in prod
+
+(defn generate-slide-padding [index]
+  (str (+ (- (/ base-width 2) (/ entry-sizing 2)) (* index entry-padding) (* index entry-sizing)) "px"))
 
 
 (defn Timeline [active enteries]
-  (print enteries)
-  [:div.SubPage {:class active}
-    [:div#slider
-      (for [entry enteries]
-        [:div.Timeline__entey
-          [:h2 (:overall entry)]])]])
-
-
-
-; (let [slider (.getElementById js/document "slider")]
-;   (.slick slider))
+  [:div.SubPage.Timeline {:class active}
+    [:div#slider {:style {:width "calc(vw * 3)"}}
+      (loop [index 0
+             output ()]
+        (if (= index (count enteries))
+          output
+          (let [entry (nth enteries index)]
+              (recur (inc index) (conj output
+                [:div.Timeline__entry {:style {:left (generate-slide-padding index)}}
+                  [:h2 (:overall entry)]])))))]])
