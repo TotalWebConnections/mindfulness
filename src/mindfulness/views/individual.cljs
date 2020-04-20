117adbe278b7b6ae6b4296969e51eb6df8dbf0b8
peterjewicz <peterjewicz@totalwebconnections.com>
2020-04-19 21:40:45 -0500

    base styling for individual page

--- src/mindfulness/views/individual.cljs
+++ src/mindfulness/views/individual.cljs
@@ -4,7 +4,13 @@
 
 
 (defn Individual [active entry]
+  (print entry)
   [:div.Page.Individual {:class active}
-    [:p {:on-click #(handle-state-change {:type "update-home-view" :value "timeline"})}"Go Back"]
-    [:h2 "im a individual page to review"]
-    [:h3 (:date entry)]])
+    [:div.individualHeader
+      [:p {:on-click #(handle-state-change {:type "update-home-view" :value "timeline"})}"Go Back"]]
+    [:div.individual.body
+      [:h3 (:date entry)]
+      [:h4 (str "Your Overall Rating: " (:overall entry))]
+      [:h4 (str "A Good Thing: " (:good entry))]
+      [:h4 (str "A Not So Good Thing: " (:bad entry))]
+      [:h4 (str "Reflection: " (:reflect entry))]]])
