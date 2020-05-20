f65ee4511308952680cc014b64015b884dcc436e
peterjewicz <peterjewicz@totalwebconnections.com>
2020-05-20 11:05:31 -0500

    add prompt support

--- src/mindfulness/components/flow/reflect.cljs
+++ src/mindfulness/components/flow/reflect.cljs
@@ -1,10 +1,13 @@
-(ns mindfulness.components.flow.reflect)
+(ns mindfulness.components.flow.reflect
+  (:require [mindfulness.services.state.prompts :refer [prompts]]))
 
 
 (defn Reflect [active handle-save]
-  (let [reflect-value (atom "")]
+  (let [prompt (nth prompts (rand-int (count prompts)))
+        reflect-value (atom {:value "" :prompt prompt})]
     (fn [active handle-save]
       [:div.SubPage.SubPageFlow.Reflect {:class active}
-        [:h2 "Here's a Fun Promp To Work Through"]
-        [:textarea {:style {:resize "none"} :on-change #(reset! reflect-value (-> % .-target .-value))}]
+        [:h3 "Here's a Fun Promp To Think About: "]
+        [:h2.Reflect_prompt (str "\""prompt"\"")]
+        [:textarea {:style {:resize "none"} :on-change #(swap! reflect-value conj {:value (-> % .-target .-value)})}]
         [:button {:on-click #(handle-save @reflect-value)}"Save!"]])))
---
+++ src/mindfulness/services/state/prompts.cljs
@@ -0,0 +1,19 @@
+(ns mindfulness.services.state.prompts)
+
+
+(def prompts [
+  "A Funny Thing That Happened Today Was..."
+  "A Quote That Inspires You."
+  "Today I Learned..."
+  "I am Grateful For..."
+  "My Life's Motto"
+  "My Favorite Book Is..."
+  "My Hero Is..."
+  "What's Your Greatest Fear and Why?"
+  "My Worst Job Was..."
+  "My Best Job Was.."
+  "City Or Country To Live?"
+  "The Most Emotional Movie I've Seen Was..."
+  "How Do You Feel About Birds?"
+  "Have You Ever Had a Pet?"
+])
