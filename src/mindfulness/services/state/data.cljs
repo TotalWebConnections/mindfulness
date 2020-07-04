97fbf289a5a660ca1421f38f38757396ed6f40be
peterjewicz <peterjewicz@totalwebconnections.com>
2020-07-04 17:41:17 -0500

    add more prompts

--- src/mindfulness/services/state/data.cljs
+++ src/mindfulness/services/state/data.cljs
@@ -2,7 +2,6 @@
 
 
 (def dailies [
-  "Take Some Time For Yourself, A Few Minutes To Reflect Each Day Is Important"
-  "Spend Sometime Outside, Fresh Air Is Always Good"
-  "Studies Show Those Who Leave Work at Work are Happy"
-])
+              "Take Some Time For Yourself, A Few Minutes To Reflect Each Day Is Important"
+              "Spend Sometime Outside, Fresh Air Is Always Good"
+              "Studies Show Those Who Leave Work at Work are Happy"])
